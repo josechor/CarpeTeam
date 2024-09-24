421bfb60191a192ec1e678e4ebe8a2663515bb63
Jose Ramon <jrpaz@boardfy.com>
2024-09-24 17:51:05 +0200

    Create first folder

---
+++ src/Header/Components/NavHeader.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+<template>
+  <nav>
+    <ul>
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ src/Header/Container/Header.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import NavHeader from '../Components/NavHeader.vue'
+</script>
+<template>
+  <header>
+    <span>CARPETEAM</span>
+    <NavHeader />
+  </header>
+</template>
